26e2330db0279ea423364aa6fc603b6c97b6cc1e
Nick Mabry <nicholasmabry@gmail.com>
2014-04-22 10:56:02 -0600

    Add basic Rails 4 support

---
+++ lib/active_support/cache/atomic_mem_cache_store.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'active_support'
+require 'atomic_store'
+
+module ActiveSupport
+  module Cache
+    class AtomicMemCacheStore < ActiveSupport::Cache::MemCacheStore
+      RAW_ARG = true
+
+      include AtomicStore
+    end
+  end
+end
--- lib/atomic_mem_cache_store.rb
+++
@@ -1,8 +0,0 @@
-require 'active_support'
-require 'atomic_store'
-
-class AtomicMemCacheStore < ActiveSupport::Cache::CompressedMemCacheStore
-  RAW_ARG = true
-
-  include AtomicStore
-end
